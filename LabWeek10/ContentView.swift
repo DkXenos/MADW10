297cb61d650fda992cfdf515ad50d94dfb1197da
DkXenos <superjason101010@gmail.com>
2026-05-03 03:14:19 +0700

    first commit

---
+++ LabWeek10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LabWeek10
+//
+//  Created by Jason TIo on 04/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LabWeek10/LabWeek10App.swift
@@ -0,0 +1,17 @@
+//
+//  LabWeek10App.swift
+//  LabWeek10
+//
+//  Created by Jason TIo on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LabWeek10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
